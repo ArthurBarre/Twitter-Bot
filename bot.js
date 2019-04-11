afb67e2959fa1cc36941076e390b47673e7258b7
ArthurBArre <arthur.barre@hetic.net>
2019-04-11 18:49:41 +0200

    tweet function ok

--- bot.js
+++ bot.js
@@ -1,9 +1,23 @@
-console.log('The bot is starting');
+console.log('The bot is actualy starting');
 
 var Twit = require('twit');
 
 var config = require('./config');
 
-console.log(config)
+// console.log(config); -> To displays ids and passwords
 
-var T = new Twit(config)
+var T = new Twit(config);
+
+var params = {
+  q: 'banana since:2011-07-11',
+  count: 100
+}
+
+T.get('search/tweets', params, gotData);
+
+function gotData(err, data, response) {
+  var tweets = data.statuses;
+  for (let i = 0; i < tweets.length; i++) {
+    console.log(tweets[i].text)
+  }
+};
---
+++ bot2.js
@@ -0,0 +1,25 @@
+console.log('The bot is actualy starting');
+
+var Twit = require('twit');
+
+var config = require('./config');
+
+// console.log(config); -> To displays ids and passwords
+
+var T = new Twit(config);
+
+var tweet = {
+  status: 'Hello world from node.js'
+}
+T.post('statuses/update', tweet, tweeted);
+
+function tweeted(err, data, response) {
+  if (err) {
+    console.log('Problems !');
+  }
+  else {
+    console.log('It\'s ok !');
+  }
+}
+
+
