e75e17a1b8225a9470ab53e8457ce9d0a9544479
ArthurBArre <arthur.barre@hetic.net>
2019-04-11 18:11:18 +0200

    config connection to twitter API

---
+++ config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  consumer_key: 'U98eG2zKyy6DYuVkxMcuC6dqD',
+  consumer_secret: '2uN57BsRzgHnUjOHU8P8v9Fu71MnGV6jNQoRTfroPyc4kUaBc5',
+  access_token: '1116353646042779648-LMKkVCXURzA4cc3FqJUPAIJaSkrKSJ',
+  access_token_secret: 'tvAW36moSj83gNocSf51k1Sa5bXMLcz6Y3MxvldIH1HCK',
+}
